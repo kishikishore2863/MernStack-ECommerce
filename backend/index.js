9158806077014c8fa894112c3bad4b37878ea70a
kishikishore008 <kishikishore.2863@gmail.com>
2024-03-04 23:54:14 +0530

    added mongodb, schemas  updated

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,38 @@
-const express = require('express');
-const fs = require('fs');
+const express =require('express');
+const app = express();
+const mongoose =require('mongoose');
+const {Admin , User , Courses} = require('./mongodb')
+const cors = require('cors')
 
-const read=fs.readFileSync('./notes/read.txt','utf-8');
-console.log(read)
+app.use(cors());
+app.use(express.json());
+
+app.post('/user/signin',(req,res)=>{
+    const {email,password} = req.body;
+   async  function  callback (user){
+        if(user){
+          res.status(403).json({message:'user Already exists'})
+        }
+        else
+        {
+          const obj = {email:email,password:password};
+          const newUser = new User(obj);
+          await newUser.save();
+          res.status(200).json({message:'user created sucessfully'})
+
+        }
+    }
+
+
+    User.findOne({email}).then(callback);
+    
+    
+});
+
+app.listen(3000,()=>{console.log("server is listening on port 3000")})
+
+
+
+
+
+
